8fac10222264b98fabfb132892679c73e781f3b8
anurag-int <ag6526803@gmail.com>
2023-08-12 04:38:05 +0530

    css added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-    <h1>Temperature Converter Website</h1>
+    <div className="w-screen h-screen bg-gradient-to-b from-blue-200 to-white flex items-center justify-center text-center">
+      <div className="bg-black bg-opacity-25 backdrop-filter backdrop-blur-lg h-[80%] w-[80%] text-[3rem] font-thin  rounded-md">
+        Temperature Converter
+      </div>
+    </div>
   );
 }
 
